709f1d22bba984ce1cba27bf571266ce3c72a217
Alt-az <235979@edu.p.lodz.pl>
2023-03-28 12:27:44 +0200

    stworzenie interfaca'u oraz klas command

---
+++ src/main/java/command/OperateToCommand.java
@@ -0,0 +1,10 @@
+package command;
+
+public class OperateToCommand implements DriverCommand{
+    private int x;
+    private int y;
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ src/main/java/command/SetPositionCommand.java
@@ -0,0 +1,11 @@
+package command;
+
+public class SetPositionCommand implements DriverCommand {
+
+    private int x;
+    private int y;
+    @Override
+    public void execute() {
+
+    }
+}
